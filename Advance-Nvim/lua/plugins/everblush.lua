5655744a37a7bfb0fb97f24494be26296efcc423
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-21 23:14:01 +0530

    Add files via upload

---
+++ Advance-Nvim/lua/plugins/everblush.lua
@@ -0,0 +1,10 @@
+return {
+  "Everblush/nvim",
+  name = "everblush",
+  lazy = false,
+  opts = {},
+  priority = 1000,
+  --config = function()
+  --	vim.cmd.colorscheme "everblush"
+  --end
+}
---
+++ Advance-Nvim/lua/plugins/tokyodark.lua
@@ -0,0 +1,10 @@
+return {
+  "tiagovla/tokyodark.nvim",
+  name = "tokyodark",
+  lazy = false,
+  opts = {},
+  priority = 1000,
+  config = function()
+  	vim.cmd.colorscheme "tokyodark"
+  end
+}
