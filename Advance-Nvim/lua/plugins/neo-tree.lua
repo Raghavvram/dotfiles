5f8b8af33f68f3950c42f6ef79a025b017d4bd33
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:29:16 +0530

    Update neo-tree.lua

--- Advance-Nvim/lua/plugins/neo-tree.lua
+++ Advance-Nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -12,18 +11,3 @@ return {
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
-=======
-return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
