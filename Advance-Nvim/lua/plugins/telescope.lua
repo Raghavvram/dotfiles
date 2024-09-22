434ce3cfef40f4d984808a51912afeacf37897ba
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:30:11 +0530

    Update telescope.lua

--- Advance-Nvim/lua/plugins/telescope.lua
+++ Advance-Nvim/lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
@@ -24,30 +23,3 @@ return {
     end,
   },
 }
-=======
-return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
-      require("telescope").load_extension("ui-select")
-    end,
-  },
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
