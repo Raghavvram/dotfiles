aa299800e84cdb45763c2e62832f2e59e9a5efe3
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:29:38 +0530

    Update nvim-tmux-navigation.lua

--- Advance-Nvim/lua/plugins/nvim-tmux-navigation.lua
+++ Advance-Nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
@@ -9,15 +8,3 @@ return {
     vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
   end,
 }
-=======
-return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    require('nvim-tmux-navigation').setup({})
-    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
-    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
-    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
-    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
-  end,
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
