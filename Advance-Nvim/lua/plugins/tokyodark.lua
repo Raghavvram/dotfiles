9a47fcf25ef87fcf7e7a8400feef41ddd50945ff
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:30:27 +0530

    Update tokyodark.lua

--- Advance-Nvim/lua/plugins/tokyodark.lua
+++ Advance-Nvim/lua/plugins/tokyodark.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
   "tiagovla/tokyodark.nvim",
   name = "tokyodark",
@@ -9,15 +8,3 @@ return {
   	vim.cmd.colorscheme "tokyodark"
   end
 }
-=======
-return {
-  "tiagovla/tokyodark.nvim",
-  name = "tokyodark",
-  lazy = false,
-  opts = {},
-  priority = 1000,
-  config = function()
-  	vim.cmd.colorscheme "tokyodark"
-  end
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
