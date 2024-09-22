caac19629e6f30e86e030d136d6bfda8031b7f6a
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:28:47 +0530

    Update git-stuff.lua

--- Advance-Nvim/lua/plugins/git-stuff.lua
+++ Advance-Nvim/lua/plugins/git-stuff.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
   {
     "tpope/vim-fugitive"
@@ -13,19 +12,3 @@ return {
     end
   }
 }
-=======
-return {
-  {
-    "tpope/vim-fugitive"
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end
-  }
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
