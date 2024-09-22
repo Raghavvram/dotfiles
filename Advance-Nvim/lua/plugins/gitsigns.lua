3745c7ab70a6e308f8ff16d84e02e172d144545d
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:29:02 +0530

    Update gitsigns.lua

--- Advance-Nvim/lua/plugins/gitsigns.lua
+++ Advance-Nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 return {
   {
     "tpope/vim-fugitive",
@@ -10,16 +9,3 @@ return {
     end,
   }
 }
-=======
-return {
-  {
-    "tpope/vim-fugitive",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  }
-}
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
