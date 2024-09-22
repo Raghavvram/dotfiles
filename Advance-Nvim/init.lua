3e1b53dfbd46d996bcf78c688d301bb33abe8ff6
Raghavvram <raghavvram.johnson@gmail.com>
2024-09-22 09:26:10 +0530

    Update init.lua

--- Advance-Nvim/init.lua
+++ Advance-Nvim/init.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -14,22 +13,3 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
-=======
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
-require("lazy").setup("plugins")
-
->>>>>>> 7e202bf651b3b9887fe5f046fb7ecf4f6784e038
