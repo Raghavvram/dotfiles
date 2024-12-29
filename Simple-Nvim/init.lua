1e31445507aba2b68415174325d9da8da3bdbf69
Raghavvram <raghavvram.johnson@gmail.com>
2024-12-30 01:11:18 +0530

    Create init.lua

---
+++ Simple-Nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.number = true
+vim.cmd('syntax on')
+vim.opt.cursorline = true
+vim.opt.ttyfast = true
+vim.cmd('filetype plugin on')
+vim.opt.relativenumber = true
