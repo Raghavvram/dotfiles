38a7b493888df126c9b26ee77c14de344405cfd7
Raghavvram <raghavvram.johnson@gmail.com>
2025-01-29 19:13:25 +0530

    Update init.lua

--- Simple-Nvim/init.lua
+++ Simple-Nvim/init.lua
@@ -1,94 +1,94 @@
 -- Plugins
-vim.cmd [[packadd packer.nvim]]
+vim.cmd [[packadd packer.nvim]] -- Ensure packer.nvim is loaded
 return require('packer').startup(function()
   use 'tpope/vim-surround' -- Surrounding ysw)
-  use 'tpope/vim-commentary' -- For Commenting gcc & gc
-  use { 'preservim/nerdtree', cmd = 'NERDTreeToggle' }
-  use 'vim-airline/vim-airline' -- Status bar
-  use 'vim-airline/vim-airline-themes'
-  use 'ryanoasis/vim-devicons' -- Developer Icons
-  use 'junegunn/fzf.vim' -- Fuzzy Finder, Needs Silversearcher-ag for :Ag
-  use 'junegunn/fzf'
-  use 'navarasu/onedark.nvim'
-  use 'mbbill/undotree'
-  use 'vim-python/python-syntax'
-  use 'alvan/vim-closetag'
-  use 'rose-pine/neovim'
-  use 'thedenisnikulin/vim-cyberpunk'
-  use 'airblade/vim-gitgutter' -- Git diff in sign column
-  use 'dense-analysis/ale' -- Linting and fixing code
+  use 'tpope/vim-commentary' -- For commenting code using gcc & gc
+  use { 'preservim/nerdtree', cmd = 'NERDTreeToggle' } -- File system explorer
+  use 'vim-airline/vim-airline' -- Status bar enhancement
+  use 'vim-airline/vim-airline-themes' -- Themes for vim-airline
+  use 'ryanoasis/vim-devicons' -- Developer icons for various plugins
+  use 'junegunn/fzf.vim' -- Fuzzy finder plugin
+  use 'junegunn/fzf' -- Fuzzy finder dependencies
+  use 'navarasu/onedark.nvim' -- One Dark color scheme
+  use 'mbbill/undotree' -- Visualize undo history
+  use 'vim-python/python-syntax' -- Enhanced Python syntax highlighting
+  use 'alvan/vim-closetag' -- Auto-closing of HTML tags
+  use 'rose-pine/neovim' -- Rose Pine color scheme
+  use 'thedenisnikulin/vim-cyberpunk' -- Cyberpunk color scheme
+  use 'airblade/vim-gitgutter' -- Show git diff in sign column
+  use 'dense-analysis/ale' -- Asynchronous linting and fixing
   use 'itchyny/lightline.vim' -- Lightweight statusline
   use { 'neoclide/coc.nvim', branch = 'release' } -- Intellisense engine
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Better syntax highlighting
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Syntax highlighting and parsing
 end)
 
 -- General Configuration
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.autoindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.encoding = 'utf-8'
-vim.opt.scrolloff = 5
-vim.opt.termguicolors = true
-vim.cmd 'colorscheme onedark'
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.mouse = 'a' -- Enable mouse support
+vim.opt.autoindent = true -- Enable automatic indentation
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.smarttab = true -- Insert 'tabstop' number of spaces when Tab is pressed
+vim.opt.encoding = 'utf-8' -- Set the encoding to UTF-8
+vim.opt.scrolloff = 5 -- Minimum number of screen lines to keep above and below the cursor
+vim.opt.termguicolors = true -- Enable 24-bit RGB colors
+vim.cmd 'colorscheme onedark' -- Set the colorscheme to One Dark
 
 -- Airline
-vim.g.airline_theme = 'onedark'
+vim.g.airline_theme = 'onedark' -- Set the airline theme to One Dark
 
 -- Nerd Tree
-vim.g.NERDTreeDirArrowExpandable = "+"
-vim.g.NERDTreeDirArrowCollapsible = "~"
-vim.g.python_highlight_all = 1
+vim.g.NERDTreeDirArrowExpandable = "+" -- Set the expandable directory arrow
+vim.g.NERDTreeDirArrowCollapsible = "~" -- Set the collapsible directory arrow
+vim.g.python_highlight_all = 1 -- Enable all Python syntax highlighting features
 
 -- Keybindings
-vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':UndotreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true }) -- Toggle NERDTree with Ctrl + t
+vim.api.nvim_set_keymap('n', '<C-l>', ':UndotreeToggle<CR>', { noremap = true }) -- Toggle UndoTree with Ctrl + l
 -- Copy to clipboard
-vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true }) -- Copy selection to system clipboard with Ctrl + c
+vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true }) -- Paste from system clipboard with Ctrl + v
+vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', { noremap = true }) -- Paste in insert mode from system clipboard with Ctrl + v
 
 -- Neovim configuration for Neovide transparency
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.6
-  vim.g.neovide_background_color = '#0f1117' .. string.format('%x', math.floor(255 * vim.g.neovide_transparency))
+  vim.g.neovide_transparency = 0.6 -- Set Neovide transparency
+  vim.g.neovide_background_color = '#0f1117' .. string.format('%x', math.floor(255 * vim.g.neovide_transparency)) -- Set Neovide background color
 end
 
 -- Set the background color to a transparent-friendly color
-vim.cmd [[highlight Normal ctermbg=none guibg=none]]
-vim.cmd [[highlight NonText ctermbg=none guibg=none]]
-vim.cmd [[highlight LineNr ctermbg=none guibg=none]]
-vim.cmd [[highlight EndOfBuffer ctermbg=none guibg=none]]
+vim.cmd [[highlight Normal ctermbg=none guibg=none]] -- Set Normal highlight group background to none
+vim.cmd [[highlight NonText ctermbg=none guibg=none]] -- Set NonText highlight group background to none
+vim.cmd [[highlight LineNr ctermbg=none guibg=none]] -- Set LineNr highlight group background to none
+vim.cmd [[highlight EndOfBuffer ctermbg=none guibg=none]] -- Set EndOfBuffer highlight group background to none
 
 -- Custom Keybindings for Pane Navigation using Alt + Arrow Keys
-vim.api.nvim_set_keymap('n', '<A-Left>', ':wincmd h<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-Right>', ':wincmd l<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-Up>', ':wincmd k<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-Down>', ':wincmd j<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-Left>', ':wincmd h<CR>', { noremap = true }) -- Move to the left pane with Alt + Left
+vim.api.nvim_set_keymap('n', '<A-Right>', ':wincmd l<CR>', { noremap = true }) -- Move to the right pane with Alt + Right
+vim.api.nvim_set_keymap('n', '<A-Up>', ':wincmd k<CR>', { noremap = true }) -- Move to the upper pane with Alt + Up
+vim.api.nvim_set_keymap('n', '<A-Down>', ':wincmd j<CR>', { noremap = true }) -- Move to the lower pane with Alt + Down
 
-vim.api.nvim_set_keymap('i', '<A-Left>', '<Esc>:wincmd h<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-Right>', '<Esc>:wincmd l<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-Up>', '<Esc>:wincmd k<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-Down>', '<Esc>:wincmd j<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-Left>', '<Esc>:wincmd h<CR>', { noremap = true }) -- Move to the left pane with Alt + Left in insert mode
+vim.api.nvim_set_keymap('i', '<A-Right>', '<Esc>:wincmd l<CR>', { noremap = true }) -- Move to the right pane with Alt + Right in insert mode
+vim.api.nvim_set_keymap('i', '<A-Up>', '<Esc>:wincmd k<CR>', { noremap = true }) -- Move to the upper pane with Alt + Up in insert mode
+vim.api.nvim_set_keymap('i', '<A-Down>', '<Esc>:wincmd j<CR>', { noremap = true }) -- Move to the lower pane with Alt + Down in insert mode
 
-vim.api.nvim_set_keymap('t', '<A-Left>', '<C-\\><C-n>:wincmd h<CR>', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-Right>', '<C-\\><C-n>:wincmd l<CR>', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-Up>', '<C-\\><C-n>:wincmd k<CR>', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-Down>', '<C-\\><C-n>:wincmd j<CR>', { noremap = true })
+vim.api.nvim_set_keymap('t', '<A-Left>', '<C-\\><C-n>:wincmd h<CR>', { noremap = true }) -- Move to the left pane with Alt + Left in terminal mode
+vim.api.nvim_set_keymap('t', '<A-Right>', '<C-\\><C-n>:wincmd l<CR>', { noremap = true }) -- Move to the right pane with Alt + Right in terminal mode
+vim.api.nvim_set_keymap('t', '<A-Up>', '<C-\\><C-n>:wincmd k<CR>', { noremap = true }) -- Move to the upper pane with Alt + Up in terminal mode
+vim.api.nvim_set_keymap('t', '<A-Down>', '<C-\\><C-n>:wincmd j<CR>', { noremap = true }) -- Move to the lower pane with Alt + Down in terminal mode
 
 -- Keybindings for Tab Management
 -- Create new tab
-vim.api.nvim_set_keymap('n', '<A-c>', ':tabnew<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-c>', ':tabnew<CR>', { noremap = true }) -- Create a new tab with Alt + c
 
 -- Close current tab
-vim.api.nvim_set_keymap('n', '<A-w>', ':tabclose<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-w>', ':tabclose<CR>', { noremap = true }) -- Close the current tab with Alt + w
 
 -- Switch to next tab
-vim.api.nvim_set_keymap('n', '<A-n>', ':tabnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-n>', ':tabnext<CR>', { noremap = true }) -- Switch to the next tab with Alt + n
 
 -- Switch to previous tab
-vim.api.nvim_set_keymap('n', '<A-p>', ':tabprevious<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-p>', ':tabprevious<CR>', { noremap = true }) -- Switch to the previous tab with Alt + p
